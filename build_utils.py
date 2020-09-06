31b8fd111fd2df7d813c3e6886629ef4d40558b1
zcribe <erlend.eelmets@gmail.com>
2020-09-06 10:11:01 +0300

    Add build utils to remove busywork

---
+++ build_utils.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+
+
+def generate_docs():
+    os.system("pdoc --html translate_po.main --force --output-dir ./docs")
+
+
+def build_dist():
+    os.system("python setup.py sdist")
+    os.system("python setup.py bdist_wheel --universal")
+
+
+def upload_to_pypi(live=False):
+    with open('.env', "r") as file:
+        if live:
+            file.readline()
+            token = file.readline()[6:]
+        else:
+            token = file.readline()[10:]
+    if live:
+        os.system(f"twine upload -r dist/* -u __token__ -p {token}")
+    else:
+        os.system(f"twine upload -r testpypi dist/* -u __token__ -p {token}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-live', type=bool, default=False)
+    arguments = parser.parse_args()
+
+    generate_docs()
+    build_dist()
+    upload_to_pypi(live=arguments.live)
--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 import setuptools
 
-with open("README", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="translate-po",  # Replace with your own username
-    version="1.0.0dev1",
+    version="1.0.3.dev1",
     author="Erlend Eelmets",
     author_email="erlend.eelmets@gmail.com",
     description="Automatic PO file translator",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['docs', 'tests', 'translated', 'untranslated']),
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Internationalization',
     ],
     python_requires='>=3.6',
     keywords='po translate automatic google',
@@ -24,5 +27,10 @@ setuptools.setup(
         'Source': 'https://github.com/zcribe/translate-po',
         'Documentation': 'https://github.com/zcribe/translate-po/docs/main.html',
         'Author': 'http://www.erlend.ee',
+    },
+    entry_points={
+        'console_scripts': [
+            'sample=sample:main',
+        ],
     },
 )
