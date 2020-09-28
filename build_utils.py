904d961bd731092199cace9f850b5c73d5ff8dc5
zcribe <erlend.eelmets@gmail.com>
2020-09-28 14:33:01 +0300

    Build script deletes old distributable

--- build_utils.py
+++ build_utils.py
@@ -1,9 +1,16 @@
 import argparse
+import glob
 import os
 
 
 def generate_docs():
     os.system("pdoc --html translate_po.main --force --output-dir ./docs")
+
+
+def delete_files_in_directory(directory):
+    files = glob.glob(directory)
+    for file in files:
+        os.remove(file)
 
 
 def build_dist():
@@ -29,6 +36,12 @@ if __name__ == '__main__':
     parser.add_argument('-live', type=bool, default=False)
     arguments = parser.parse_args()
 
+    print("### Generating documentation")
     generate_docs()
+    print("### Cleaning distributable directory")
+    delete_files_in_directory("./dist/*")
+    print("### Building the distributable")
     build_dist()
+    print("### Uploading the distributable to PyPi")
     upload_to_pypi(live=arguments.live)
+    print("### Distributable uploaded")
